8ccd75e295b09406e723a616a8858e6e1449b1f7
MembreC57 <membre@c57.fr>
2018-10-17 20:03:03 +0200

    Base Page HTML

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Sitetest</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <h1>Site Test</h1>
+
+  <h3>My subtitle</h3>
+  <p>Notre première page Web</p>
+
+  <ol>
+    <li>Uno</li>
+    <li>Deux</li>
+    <li>Trois 333 456 789</li>
+  </ol>
+
+  <hr>
+
+  <?php
+
+  include 'a.php';
+
+  ?>
+
+  <script src="app.js"></script>
+</body>
+
+</html>
